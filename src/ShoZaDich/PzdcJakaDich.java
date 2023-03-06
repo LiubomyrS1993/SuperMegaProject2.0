0228d50e19b031e292f84f5513452f1e75f75c13
liubomyr23 <liubomyr.suduk@lendinvest.com>
2023-03-06 13:48:12 +0200

    new update

---
+++ src/ShoZaDich/PzdcJakaDich.java
@@ -0,0 +1,16 @@
+package ShoZaDich;
+
+public class PzdcJakaDich {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        a = 10;
+        b = 20;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
