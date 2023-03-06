bf5f389014785ea032cc93c5dce071c984a0e185
liubomyr23 <liubomyr.suduk@lendinvest.com>
2023-03-06 14:06:43 +0200

    new update

---
+++ src/ShoZaDich/MyFirstHome1.java
@@ -0,0 +1,19 @@
+package ShoZaDich;
+
+public class MyFirstHome1 {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        a = 10;
+        b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("a = " + b);
+        System.out.println("b = " + a);
+    }
+}
