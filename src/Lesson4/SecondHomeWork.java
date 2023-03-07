2284336d0ecae5a1fc416766696b8e9b19dc2108
liubomyr23 <liubomyr.suduk@lendinvest.com>
2023-03-07 20:10:05 +0200

    new update

--- src/Lesson4/SecondHomeWork.java
+++ src/Lesson4/SecondHomeWork.java
@@ -4,44 +4,29 @@ public class SecondHomeWork {
 
     public static void main(String[] args) {
 
-        int foodPerCats = howMuchCatFoodPerMonth();
+        int foodPerCats = howMuchCatFoodPerMonth(8, 30, 3, 50);
         System.out.println("Cat food is needed per month: " + foodPerCats + " grams");
 
-        int foodPerDogs = howMuchDogFoodPerMonth();
+        int foodPerDogs = howMuchDogFoodPerMonth(24, 30, 3, 50);
         System.out.println("Dog food is needed per month :" + foodPerDogs + " grams");
 
-        int foodCostPerMonth = howMuchMoneyNeedPerMonth();
+        int foodCostPerMonth = howMuchMoneyNeedPerMonth(32, 30, 3, 2);
         System.out.println("The money is needed per month for the maintenance of the shelter: " + foodCostPerMonth + " $");
 
     }
 
-    public static int howMuchCatFoodPerMonth(){
-        int cats = 8;
-        int daysInMonth = 30;
-        int threeTimesPerDay = 3;
-        int portion = 50;
+    public static int howMuchCatFoodPerMonth(int cats, int daysInMonth, int threeTimesPerDay, int portion){
         int catsFoodMonth = portion * threeTimesPerDay * cats * daysInMonth;
-
         return catsFoodMonth;
     }
 
-    public static int howMuchDogFoodPerMonth(){
-        int dogs = 24;
-        int daysInMonth = 30;
-        int threeTimesPerDay = 3;
-        int portion = 50;
+    public static int howMuchDogFoodPerMonth(int dogs, int daysInMonth, int threeTimesPerDay, int portion){
         int foodPerDogs = portion * threeTimesPerDay * dogs * daysInMonth;
-
         return foodPerDogs;
     }
 
-    public static int howMuchMoneyNeedPerMonth(){
-        int dogsAndCats = 24 + 8;
-        int daysInMonth = 30;
-        int threeTimesPerDay = 3;
-        int cost = 2;
+    public static int howMuchMoneyNeedPerMonth(int dogsAndCats, int daysInMonth, int threeTimesPerDay, int cost){
         int moneyPerMonth = cost * threeTimesPerDay * dogsAndCats * daysInMonth;
-
         return moneyPerMonth;
     }
 }
