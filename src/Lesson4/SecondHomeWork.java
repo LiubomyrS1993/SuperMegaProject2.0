b6c2b16ee07084bd8652e2a486761807affa99a0
liubomyr23 <liubomyr.suduk@lendinvest.com>
2023-03-08 14:16:21 +0200

    new update

--- src/Lesson4/SecondHomeWork.java
+++ src/Lesson4/SecondHomeWork.java
@@ -4,28 +4,28 @@ public class SecondHomeWork {
 
     public static void main(String[] args) {
 
-        int foodPerCats = howMuchCatFoodPerMonth(8, 30, 3, 50);
+        int foodPerCats = countCatFoodPerMonth(8, 30, 3, 50);
         System.out.println("Cat food is needed per month: " + foodPerCats + " grams");
 
-        int foodPerDogs = howMuchDogFoodPerMonth(24, 30, 3, 50);
+        int foodPerDogs = countDogFoodPerMonth(24, 30, 3, 50);
         System.out.println("Dog food is needed per month :" + foodPerDogs + " grams");
 
-        int foodCostPerMonth = howMuchMoneyNeedPerMonth(32, 30, 3, 2);
+        int foodCostPerMonth = countMoneyNeedPerMonth(32, 30, 3, 2);
         System.out.println("The money is needed per month for the maintenance of the shelter: " + foodCostPerMonth + " $");
 
     }
 
-    public static int howMuchCatFoodPerMonth(int cats, int daysInMonth, int threeTimesPerDay, int portion){
+    public static int countCatFoodPerMonth(int cats, int daysInMonth, int threeTimesPerDay, int portion){
         int catsFoodMonth = portion * threeTimesPerDay * cats * daysInMonth;
         return catsFoodMonth;
     }
 
-    public static int howMuchDogFoodPerMonth(int dogs, int daysInMonth, int threeTimesPerDay, int portion){
+    public static int countDogFoodPerMonth(int dogs, int daysInMonth, int threeTimesPerDay, int portion){
         int foodPerDogs = portion * threeTimesPerDay * dogs * daysInMonth;
         return foodPerDogs;
     }
 
-    public static int howMuchMoneyNeedPerMonth(int dogsAndCats, int daysInMonth, int threeTimesPerDay, int cost){
+    public static int countMoneyNeedPerMonth(int dogsAndCats, int daysInMonth, int threeTimesPerDay, int cost){
         int moneyPerMonth = cost * threeTimesPerDay * dogsAndCats * daysInMonth;
         return moneyPerMonth;
     }
---
+++ src/Lesson5/SecondTaskHomeWork.java
@@ -0,0 +1,40 @@
+package Lesson5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SecondTaskHomeWork {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Provide your height of baggage(centimeter): ");
+        int height = Integer.parseInt(reader.readLine());
+        System.out.println("your height of baggage is: " + height);
+
+        System.out.println("Provide your width of baggage(centimeter): ");
+        int width = Integer.parseInt(reader.readLine());
+        System.out.println("your width of baggage is: " + width);
+
+        System.out.println("Provide your length of baggage(centimeter): ");
+        int length = Integer.parseInt(reader.readLine());
+        System.out.println("your length of baggage is: " + length);
+
+
+        if(height <= 40 && width <= 20 && length <= 55){
+
+            System.out.println("Cool bro your bag/backpack is suitable for hand luggage");
+
+        }
+        else if (height <= 53 && width <= 22 && length <= 40){
+            System.out.println("Cool bro your bag/backpack is suitable for hand luggage ");
+        }
+        else{
+            System.out.println("Yom must pay for your luggage");
+        }
+
+    }
+
+}
